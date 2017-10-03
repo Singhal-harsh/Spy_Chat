5e9c74a4bfdb0ac4b4a5971222411359a940b3b9
Singhal-harsh <Singhal.harsh2@gmail.com>
2017-10-04 03:11:28 +0530

    SpyChat: Module 2 completed,add friend and status messages

--- main.py
+++ main.py
@@ -1,11 +1,70 @@
 from spy_details import spy_name,spy_rating,spy_age,spy_salutation
 
+STATUS_MESSAGES = ["Busy","Available","Imperfecton is beautiful","Spy work is the best!"]
+friends_name = []
+friends_sal = []
+friends_age = []
+friends_rating=[]
+
+
 def start_chat(spy_name,spy_age,spy_rating):
-    menu_choices = "What do you want to do..\n1.Add Status message\n"
-    choice = raw_input(menu_choices)
-    while(choice=='1'):
-        print "Status update function called\n"
+    menu_show = True
+    current_S_message = None
+
+    menu_choices = "What do you want to do..\n1.Add Status message\n2.Add Friend\n3.Exit Application\n"
+
+    while(menu_show):
         choice = raw_input(menu_choices)
+        if(choice=='1'):
+            print "Status update function called"
+            current_S_message = add_status(current_S_message)
+            print "Your Status message is: "+current_S_message+"\n"
+        elif choice=='2':
+            print "Add friend function called"
+            no_Friends = add_friend()
+            print "You have %d friend(s)!"%no_Friends
+        else:
+            print "Thank you for using SpyChat!"
+            menu_show = False
+
+def add_status(current_S_message):
+    if current_S_message!=None:
+        print "Your current status is: "+ current_S_message
+    else:
+        print "You have not set any status!"
+
+    response = raw_input("Do you want to select status from old statuses..\n")
+
+    if(response.upper()=='N'):
+        new_S_msg = raw_input("Your status message that you want to add: ")
+        if len(new_S_msg)>0:
+            updated_S_msg = new_S_msg
+        STATUS_MESSAGES.append(updated_S_msg)
+
+    else:
+        i = 1
+        for msg in STATUS_MESSAGES:
+            print str(i)+"."+msg
+            i = i+1
+        msg_choice = int(raw_input("Which message do you want to choose.."))
+        if len(STATUS_MESSAGES)>=msg_choice :
+            updated_S_msg = STATUS_MESSAGES[msg_choice-1]
+
+    return  updated_S_msg
+
+def add_friend():
+    new_name = raw_input("Input your friends name: ")
+    new_sal = raw_input("Is your friend Mr. or Ms: ")
+    new_age = int(raw_input("Age of your spy friend: "))
+    new_rating = raw_input("Enter Spy friends' spy rating: ")
+    if(len(new_name)>0 and new_age<50 and new_age>12):
+        friends_name.append(new_name)
+        friends_sal.append(new_sal)
+        friends_age.append(new_age)
+        friends_rating.append(new_rating)
+    else:
+        print "Invalid name or not appropriate age"
+    return len(friends_name)
 
 print "Hello! , Lets get started"
 ans = raw_input("Do you want to continue as "+spy_salutation+"."+spy_name+"..")
@@ -44,6 +103,7 @@ else:
         print "Authentication complete. Welcome " + spy_name + " age: " + str(spy_age) + " with rating: " + str(
             spy_rating)
         print "Glad to have you with us!"
+        start_chat(spy_name, spy_age, spy_rating)
 
     else:
         print "You are not of correct age!"
--- spy_details.py
+++ spy_details.py
@@ -2,3 +2,4 @@ spy_name = "Harsh"
 spy_age = 18
 spy_salutation = "Mr"
 spy_rating = 4.9
+spy_online = True
