9183dc719ae9ac91aa1452cff7a43c73bf8471a2
Singhal-harsh <Singhal.harsh2@gmail.com>
2017-10-10 00:51:52 +0530

    Added sleep function and termcolor module

--- Validity.py
+++ Validity.py
@@ -1,12 +1,18 @@
+import time
+
 def check_name(name):
     while(len(name)==0):
+        time.sleep(0.3)
         print "Invalid Name!"
+        time.sleep(0.3)
         name = raw_input("Please Enter correct name: ")
     return name
 
 def check_Sal(sal):
     while(len(sal)==0):
+        time.sleep(0.3)
         print "Inavlid Salutation!"
+        time.sleep(0.3)
         sal = raw_input("Enter correct Salutation: ")
     return sal
 
@@ -17,7 +23,9 @@ def check_age(age):
         return False
 
 def Valid_Password(p):
-      while len(p)<8 and len(p)>15:
+      while (len(p)<8 or len(p)>15) :
+         time.sleep(0.3)
          print "Invalid length of Password"
+         time.sleep(0.3)
          p = raw_input("Enter Password: ")
       return p
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from steganography.steganography import Steganography
-from datetime import datetime
+import time
 from spy_details import spy,friends,Spy,Chat_msg
 from users_details import Users,User
-
+from termcolor import colored
 from Validity import *
 import csv
 
@@ -12,16 +12,20 @@ def check_special_words(s_tobeSplit):
     secret_text_words = s_tobeSplit.split()
     for word in secret_text_words:
         if word == "SOS":
-            print "FBI called, Emergency respond team will be there ASAP."
+            time.sleep(0.3)
+            print colored("FBI called, Emergency respond team will be there ASAP.","red")
         if word == "EXTRACT":
-            print "Extraction team sent to your location."
+            time.sleep(0.3)
+            print colored("Extraction team sent to your location.","red")
     count = 0
     for words in secret_text_words:
         if count + 1 < len(secret_text_words):
             if secret_text_words[count].upper() == "SAVE" and secret_text_words[count + 1].upper() == "ME":
-                print "FBI called, Emergency respond team will be there ASAP."
+                time.sleep(0.3)
+                print colored("FBI called, Emergency respond team will be there ASAP.","red")
             if secret_text_words[count].upper() == "HELP" and secret_text_words[count + 1].upper() == "ME":
-                print "Another agent is coming to help you ASAP."
+                time.sleep(0.3)
+                print colored("Another agent is coming to help you ASAP.","red")
         count = count+1
 
 def load_friends():
@@ -40,52 +44,61 @@ def load_users():
 
 def load_chat(ch):
     name_friend = friends[ch].name
-    print "Chats with %s are: "%name_friend
+
+    red_name_friend = colored(name_friend,'red')
+    time.sleep(0.3)
+    print "Chats with %s are:"%red_name_friend
     with open("chats.csv","rb") as chats_data:
         reader = csv.reader(chats_data)
         check = False
         for row in reader:
             if(row[0]==name_friend):
                 check = True
-                print row[1]+"\nTime: "+row[2]
+                time.sleep(0.3)
+                print row[1]
+                Time = row[2]
+                blue_time = colored(Time,"blue")
+                time.sleep(0.3)
+                print (blue_time+"\n")
 
         if check==False :
+            time.sleep(0.3)
             print "You have no chats with the selected friend!"
 
 def send_msg():
     friend_ch = select_friend()
+    time.sleep(0.3)
     original_img = raw_input("What is the name of the image? ")
     output_path = "output.jpg"
+    time.sleep(0.3)
     text = raw_input("What is the secret message? ")
     Steganography.encode(original_img,output_path,text)
 
     new_chat = Chat_msg(text,True)
-    time = new_chat.time
-    time = time.strftime("%b %d %Y %H:%M:%S")
+    Time = new_chat.time
+    Time = Time.strftime("%b %d %Y %H:%M:%S")
     friends[friend_ch].chats.append(new_chat)
     Name = friends[friend_ch].name
     with open("chats.csv","ab") as chats_data:
         writer = csv.writer(chats_data)
-        writer.writerow([Name,text,time])
+        writer.writerow([Name,text,Time])
+    time.sleep(0.3)
     print "Your message is now crypted!"
 
 def read_msg():
     sender = select_friend()
+    time.sleep(0.3)
     output_path = raw_input("What is the name of the file? ")
     try:
         secret_text = Steganography.decode(output_path)
 
         new_chat = Chat_msg(secret_text, False)
-
+        friends[sender].chats.append(new_chat)
         check_special_words(secret_text)
-
-        print "Your message is now decrypted!"
-        for chat in friends[sender].chats:
-            if (chat.sent_by_me):
-                a = "right"
-            else:
-                a = "left"
-            print chat.msg +" " +  a
+        time.sleep(0.3)
+        print "Your message is now decrypted!,It is: "
+        time.sleep(0.6)
+        print colored(secret_text,"blue")
     except IOError:
         print "File Not Found"
 
@@ -94,58 +107,73 @@ def display_friends() :
         print "You have no friends!"
         return 0
     for friend in friends:
-        print "%s.%s of age %d with rating of %.1f is online!" % (friend.sal, friend.name, friend.age, friend.rating)
+        t = "%s.%s of age %d with rating of %.1f is online!" % (friend.sal, friend.name, friend.age, friend.rating)
+        green_t = colored(t,"green")
+        print green_t
+        time.sleep(0.4)
 
 def start_chat(spy) :
     spy.name = spy.sal + ". " + spy.name
+    time.sleep(0.3)
     print "Authentication complete,Welcome " \
           +spy.name + " of age " + str(spy.age) + " with rating of " + str(spy.rating)
     print "Glad to have you with us!"
 
     load_friends()
+    time.sleep(0.6)
     display_friends()
-    print "First, Lets show you your chat history!\nSelect a friend whom you want to see your chats!"
+
+    print "First, Lets show you your chat history!\nSelect a friend with whom you want to see your chats!"
     ch = select_friend()
     load_chat(ch)
 
     menu_show = True
-    menu_choices = "What do you want to do..\n1.Add Status message\n2.Add Friend\n3.Select Friend\n"
-    menu_choices = menu_choices+"4.Send Message\n5.Read Message\n6.Exit Application\n"
+    menu_choices = "What do you want to do..\n1.Add Status message\n2.Add Friend\n3.Read Chats\n"
+    menu_choices = menu_choices+"4.Send a Message\n5.Read a Message\n6.Exit Application\n"
     while(menu_show):
+        time.sleep(0.5)
         choice = raw_input(menu_choices)
         if(choice=='1'):
-            print "Status update function called.."
             spy.current_S_msg = add_status(spy.current_S_msg)
+            time.sleep(0.3)
             print "Your Status message is: "+spy.current_S_msg+"\n"
         elif (choice=='2'):
-            print "Add friend function called..."
             no_Of_Friends = add_friend()
+            time.sleep(0.3)
             print "You have %d friend(s)!"%no_Of_Friends
         elif(choice=='3'):
-            print "Select Friend called.."
-            t = select_friend()
+            time.sleep(0.3)
+            print "Select a friend with whom you want to see your chats!"
+            ch1 = select_friend()
+            load_chat(ch1)
         elif (choice=='4'):
-            print "Send message called.."
             send_msg()
         elif(choice=='5'):
-            print "Read message called.."
             read_msg()
         elif choice=='6':
+            time.sleep(0.3)
             print "Thank you for using SpyChat!"
             menu_show = False
         else:
+            time.sleep(0.3)
             print "Wrong input!"
+            print "Closing application"
+            time.sleep(0.5)
             exit()
 
 def add_status(current_S_msg) :
     if current_S_msg!=None:
+        time.sleep(0.3)
         print "Your current status is: "+ current_S_msg
     else:
+        time.sleep(0.3)
         print "You have not set any status!"
 
+    time.sleep(0.3)
     response = raw_input("Do you want to select status from old statuses?(Y/N)\n")
 
     if(response.upper()=='N'):
+        time.sleep(0.3)
         new_S_msg = raw_input("Your status message that you want to add: ")
         if len(new_S_msg)>0:
             updated_S_msg = new_S_msg
@@ -156,22 +184,27 @@ def add_status(current_S_msg) :
         for msg in STATUS_MESSAGES:
             print str(i)+"."+msg
             i = i+1
+            time.sleep(0.3)
         msg_choice = int(raw_input("Which message do you want to choose.."))
         if len(STATUS_MESSAGES)>=msg_choice :
             updated_S_msg = STATUS_MESSAGES[msg_choice-1]
     else:
+        time.sleep(0.3)
         print "Wrong input!"
 
     return updated_S_msg
 
 def add_friend():
     new_friend = Spy("","",0,0.0)
-
+    time.sleep(0.3)
     new_friend.name = raw_input("Input your friends name: ")
     new_friend.name = check_name(new_friend.name)
+    time.sleep(0.3)
     new_friend.sal = raw_input("Is your friend Mr or Ms: ")
     new_friend.sal= check_Sal(new_friend.sal)
+    time.sleep(0.3)
     new_friend.age = int(raw_input("Age of your spy friend: "))
+    time.sleep(0.3)
     new_friend.rating= input("Enter your spy friend's spy rating: ")
     if(check_age(new_friend.age)):
         friends.append(new_friend)
@@ -179,6 +212,7 @@ def add_friend():
             writer = csv.writer(friends_data)
             writer.writerow([new_friend.name,new_friend.sal,new_friend.age,new_friend.rating,new_friend.is0nline])
     else:
+        time.sleep(0.3)
         print "Invalid name or not appropriate age!"
 
     return len(friends)
@@ -188,6 +222,7 @@ def select_friend():
     for friend in friends:
         print str(i)+". "+friend.sal+"."+friend.name
         i = i+1
+        time.sleep(0.4)
     index = input("Select a friend from the above list by entering the number before the name? ")
     while(index>len(friends)):
         print "Invalid input"
@@ -199,6 +234,7 @@ def select_user():
     for user_spy in Users:
         print str(i)+"."+user_spy.username
         i = i+1
+        time.sleep(0.4)
     index = input("Select from above which user are you? ")
     while (index > len(Users)):
         print "Invalid input"
@@ -213,6 +249,7 @@ if ans.upper()=='Y':
     #start with the app
     counter = 5
     while(counter>0):
+        time.sleep(0.1)
         p = raw_input("Password: ")
         if (p == "HarshS1234"):
             start_chat(spy)
@@ -227,9 +264,11 @@ elif ans.upper()=='N':
     if choice.upper()=='Y':
         load_users()
         index = select_user()
+        time.sleep(0.3)
         print "Hello %s!"%Users[index].username
         counter = 5
         while (counter > 0):
+            time.sleep(0.3)
             p = raw_input("Enter Password: ")
             if (p == Users[index].password):
                 temp_user = Users[index]
@@ -240,32 +279,41 @@ elif ans.upper()=='N':
                 start_chat(spy)
                 break
             else:
+                time.sleep(0.3)
                 print "Incorrect Password!"
                 print "%d tries remaining!!" % (counter - 1)
             counter = counter - 1
 
     elif choice.upper() == 'N':
+        time.sleep(0.3)
         print "SpyChat promises to be the best chat application."
         print "Our sign up is very easy and efficient!"
+        time.sleep(0.3)
         spy.name = raw_input("Kindly enter your name: ")
         spy.name = check_name(spy.name)
+        time.sleep(0.3)
         spy.sal = raw_input("What should we call you? ")
         spy.sal = check_Sal(spy.sal)
-
+        time.sleep(0.3)
         print "Hello! " + spy.sal + ". " + spy.name + ", Welcome to spy chat!"
-
+        time.sleep(0.3)
         spy.age = input("Kindly enter your age: ")
         if check_age(spy.age):
             spy.rating = input("How are you rated? ")
             if spy.rating >= 4.7:
+                time.sleep(0.3)
                 print "You are one of the best!"
             elif (spy.rating > 3.0 and spy.rating < 4.7):
+                time.sleep(0.3)
                 print "There is still a chance of improvement!"
             else:
+                time.sleep(0.3)
                 print "You need to work a lot more harder"
 
             username = spy.name+"@SpyChat"
+            time.sleep(0.3)
             print "Your username is : " + username
+            time.sleep(0.3)
             password_newUser = raw_input("Please Enter a password for your account(8-15chars): ")
             password_newUser = Valid_Password(password_newUser)
             new_user = User(username,password_newUser,spy.name,spy.sal,spy.age,spy.rating)
@@ -275,15 +323,19 @@ elif ans.upper()=='N':
                 writer.writerow([new_user.username,new_user.password,spy.name,spy.sal,spy.age,spy.rating])
             start_chat(spy)
         else:
+            time.sleep(0.3)
             print "You are not of correct age!"
             exit()
 
     else:
+        time.sleep(0.3)
         print "Wrong input! "
         exit()
 else:
+    time.sleep(0.3)
     print "Wrong Input!"
     print "Closing application!"
+    time.sleep(0.7)
     exit()
 
 
